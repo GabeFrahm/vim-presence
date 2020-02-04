a6d44b1142c18b6179230d4d8bf5a4b56fdd3e12
Ryan Densmore <rdensmore365@gmail.com>
2020-02-04 12:28:20 -0500

    hi

--- python3/sample.py
+++ python3/sample.py
@@ -31,16 +31,11 @@ def DiscordConnect():
     rp = Presence('672929723382235136') # Client ID
     rp.connect() # Start Handshake loop
     print("Achievement Get: Discord Connected!")
-    
+
 def SetPresence():
     rp.update(
-<<<<<<< HEAD
         state = modes[vim.eval('mode()')],
         details = f'Editing a {fileType} file!',
-=======
-        state = f'This is a stateeeee',
-        details = f'Editing a {fileType} file',
->>>>>>> 9f3c70f8b5707d62511b87bc60e6cc2916b3e67b
         large_image = fileType if fileType in files else 'default',
         large_text = fileType,
         small_image = 'vim',
